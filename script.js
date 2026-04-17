9e33476905c84f1aa05de23f7627a954a9abf3b5
Ammar Yasir <yashirgaming@gmail.com>
2026-04-17 22:55:37 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const left = document.querySelector('#left')
+const right= document.querySelector('#right')
+const fourth = document.querySelector('.fourth-review')
+
+right.addEventListener('click',()=>{
+     fourth.style.transform = `translateX(-100%)`
+})
+
 
 
 
